18b065069684e0726c36d2cf68af03eb8cfcebe0
Vishal Katiyar <vishalkatiyar@vizexperts.com>
2020-08-09 11:02:46 +0530

    play button text changes

--- src/components/result.js
+++ src/components/result.js
@@ -3,7 +3,7 @@ import React from 'react';
 const result = ({score,playAgain}) => (
     <div className="score-board">
         <div className="score">your score {score}/5 correct answer</div>
-         <button className="playBtn" onClick={playAgain}>play again</button>
+         <button className="playBtn" onClick={playAgain}>play again!</button>
     </div>
 );
 
